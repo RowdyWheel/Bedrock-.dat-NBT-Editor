97543208efa2af6122da41667d05a520c1f1d5a6
RowdyWheel <rowdywheel62@gmail.com>
2025-06-26 14:07:01 +0530

    Update service-worker.ts

--- src/service-worker/service-worker.ts
+++ src/service-worker/service-worker.ts
@@ -1,58 +1,55 @@
-/// <reference lib="webworker" />
-export {}; // prevent global scope conflicts
-
 import { version as VERSION } from "../../package.json";
 
-// Force self to be recognized as a ServiceWorker
-declare const self: ServiceWorkerGlobalScope;
+declare var self: ServiceWorkerGlobalScope;
+declare const clients: Clients;
 
 const NAME = "Flatlands";
 const CACHE_NAME = `${NAME} v${VERSION}` as const;
 
-self.addEventListener("activate", (event) => {
-  (event as ExtendableEvent).waitUntil(removeOutdatedVersions());
+self.addEventListener("activate",event => {
+  event.waitUntil(removeOutdatedVersions());
 });
 
-self.addEventListener("fetch", (event) => {
-  const fetchEvent = event as FetchEvent;
-  fetchEvent.respondWith(matchRequest(fetchEvent.request));
+self.addEventListener("fetch",event => {
+  event.respondWith(matchRequest(event.request));
 });
 
 /**
  * Clears out old versions of the app from Cache Storage.
- */
+*/
 async function removeOutdatedVersions(): Promise<void> {
   const keys = await caches.keys();
 
-  await Promise.all(keys.map(async (key) => {
+  await Promise.all(keys.map(async key => {
     const isOutdatedVersion = key.startsWith(NAME) && key !== CACHE_NAME;
-    if (isOutdatedVersion) {
+
+    if (isOutdatedVersion){
       await caches.delete(key);
     }
   }));
 
-  // self is now properly typed, this works
-  await self.clients.claim();
+  await clients.claim();
 }
 
 /**
- * Matches a network request with its cached counterpart from Cache Storage.
- * If not cached, fetch from network, store in cache, and return it.
- */
+ * Matches a network request with it's cached counterpart from Cache Storage.
+ * 
+ * If it hasn't been cached yet, it will fetch the network for a response, cache a clone, then return the response.
+*/
 async function matchRequest(request: Request): Promise<Response> {
   let response = await caches.match(request);
   if (response !== undefined) return response;
 
   response = await fetch(request);
-  await cacheRequest(request, response);
+  await cacheRequest(request,response);
 
   return response;
 }
 
 /**
  * Adds a network request and response to Cache Storage.
- */
+*/
 async function cacheRequest(request: Request, response: Response): Promise<void> {
   const cache = await caches.open(CACHE_NAME);
-  await cache.put(request, response.clone());
-    }
+  await cache.put(request,response.clone());
+                      }
